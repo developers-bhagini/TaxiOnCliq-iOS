b1f8089baf4db6a24da5fbac8cde3e4b8f3d99a8
venkatesh <developers.bhagini@gmail.com>
2016-11-20 23:50:29 +0530

    "code "

---
+++ TaxiOnCliq/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaxiOnCliq
+//
+//  Created by Apple on 20/11/16.
+//  Copyright © 2016 Venkatesh Mandapati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
